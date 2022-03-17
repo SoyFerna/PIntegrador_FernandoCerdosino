7dc1dd7cd106311d15460e890f8ab8736ae6e719
Ferna Cerdosino <SoyFerna@users.noreply.github.com>
2022-03-17 14:20:39 -0300

    Aplicado de logos y creación de respectivas carpetas

--- PIntegrador_FernandoCerdosino/src/app/app.module.ts
+++ PIntegrador_FernandoCerdosino/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { SocialmediaComponent } from './components/socialmedia/socialmedia.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    SocialmediaComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
